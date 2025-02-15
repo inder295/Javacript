8ef1c307d6d6ff3fa2eca5391103b7eb5e89ece2
INDERPREET SINGH <inderpreetsingh2017@gmail.com>
2025-02-15 14:15:29 +0530

    shallow copy

---
+++ objects/MCI.js
@@ -0,0 +1,12 @@
+//memory allocation in variable.
+
+let name="Inder";
+let name2=name;
+
+console.log(name2);
+
+
+name='karan';
+console.log(name);
+
+//name2 value not get changed because name2 copies the value from name1 not the refernce address. 
---
+++ objects/MCI2.js
@@ -0,0 +1,14 @@
+//memory alloaction in objects
+
+let p1={
+    name:"Inderpreet"
+}
+
+let p2=p1;
+console.log(p2);
+
+p2.name="arshpreet"
+
+console.log(p1);
+console.log(p2);
+
