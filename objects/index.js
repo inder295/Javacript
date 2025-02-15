116d08e163a4f755f37a9ec796958ec4a23b62be
INDERPREET SINGH <inderpreetsingh2017@gmail.com>
2025-02-15 11:47:17 +0530

    object introduction

--- objects/index.js
+++ objects/index.js
@@ -1,2 +1,11 @@
 let i=10;
-console.log(i);
+//console.log(i+5);
+
+const person={
+   name: "Anonymous",
+   age:30,
+   blood_group:"ABC",
+   designation: "Data analytics"
+}
+
+console.log(person.designation);
