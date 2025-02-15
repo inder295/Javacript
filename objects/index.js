311b40cc2570fca4b7492e4d23dee039454878a4
INDERPREET SINGH <inderpreetsingh2017@gmail.com>
2025-02-15 12:33:01 +0530

    object basics done

--- objects/index.js
+++ objects/index.js
@@ -5,7 +5,27 @@ const person={
    name: "Anonymous",
    age:30,
    blood_group:"ABC",
-   designation: "Data analytics"
+   designation: "Data analytics",
+   isMarried:false,
+   getFullname: function () {
+    return "Anonymous user"
+   },
+   address:{
+     hno :1,
+     street: "Nehri place street",
+     full_Address:{
+        line1:{
+            flat: 1
+        },
+        line2:{
+            place: "kalkaji",
+        },
+        line3:{
+          country : "India"
+        }
+     },
+     hadcrush: Infinity
+   }
 }
 
-console.log(person.designation);
+console.log(person.hadcrush);
