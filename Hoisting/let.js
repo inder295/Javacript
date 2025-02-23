25bae540b6b4c006ffb209958208e5cf02045fdb
INDERPREET SINGH <inderpreetsingh2017@gmail.com>
2025-02-24 00:18:51 +0530

    hoisting on var and let/const added

---
+++ Hoisting/let.js
@@ -0,0 +1,13 @@
+//let/const -> hoist hoyenge
+
+//but js is design in such a that we cannot acess it.
+
+console.log('age is',age);
+console.log('----dead zone------');
+
+let age =10;
+
+console.log(age);
+
+
+
---
+++ Hoisting/var.js
@@ -0,0 +1,14 @@
+console.log('Age is ',age);
+
+test(); //memory me function save hai
+
+function test(){
+    console.log('Function age is ',age);    
+}
+
+
+var age=10;
+
+console.log('age is ',age);
+
+
